4030df415711589d1c53af271d70177403d1a093
Luks <51793738+0xLuks@users.noreply.github.com>
2022-08-23 20:52:03 +0200

    Update index.js

--- JS/HTTP_redirect_server/index.js
+++ JS/HTTP_redirect_server/index.js
@@ -1,15 +1,28 @@
 #!/usr/bin/env node
 
 const express = require('express');
+const { program } = require('commander');
 const app = express();
 const port = 3000;
-const host = "127.0.0.1:3000/"
+
+program 
+    .description('Redirects all HTTP traffic locally')
+    .option('-r, --destinationUrl <path>', 'the url that will be redirected')
+    .option('-p, --port <port>, "specify the port of the url", Number, 3000');
+
+program.parse();
+
+const options = program.opts();
+const destUrl = options.destinationUrl;
+const destPort = options.port;
 
 app.get("/", (req,res) => {
-    res.redirect(302, `http://${host}:${port}`);
+    res.redirect(302, `http://${destUrl}:${destPort}`);
 });
 
 app.listen(port, () => {
-    console.log(`Listening on port 80`);
-    app.listen(80);
+    console.log(`[+] Listening on port 80`);
+    console.log(`[+] Redirected to localhost on port ${port}`);
 });
+
+app.listen(80);
