bcc4f4aca9b8b71a40e9f8e8877726e4525845a5
Luks <51793738+0xLuks@users.noreply.github.com>
2022-08-23 22:11:45 +0200

    Update redir.js

--- JS/HTTP_redirect_server/redir.js
+++ JS/HTTP_redirect_server/redir.js
@@ -4,7 +4,28 @@
 At the moment, the option for the "-p" port does not work
 It necessary to specify the port manuelly in the URL
 
-Ex: node redir.js -r 127.0.0.1:3000/api/.....   
+Ex: node redir.js -r 127.0.0.1:3000/api/.....
+
+TO-DO:
+
+/* Commander new syntax
+const { program } = require('commander');
+program.version('0.0.1');
+
+- Cmd - 
+program
+.command('-r, --destinationUrl <path>', 'the url that will be redirected')
+.command('-p, --port <port>', 'specify the port of the url')
+
+- Options - 
+program
+.description('Redirects all HTTP traffic locally')
+
+const options = program.opts();
+const destUrl = command.destinationUrl;
+const destPort = command.port;
+
+program.parse(process.argv);
 */
 
 const express = require('express');
@@ -22,6 +43,26 @@ program.parse();
 const options = program.opts();
 const destUrl = options.destinationUrl;
 const destPort = options.port;
+
+/* Commander new syntax
+const { program } = require('commander');
+program.version('0.0.1');
+
+- Cmd - 
+program
+.command('-r, --destinationUrl <path>', 'the url that will be redirected')
+.command('-p, --port <port>', 'specify the port of the url')
+
+- Options - 
+program
+.description('Redirects all HTTP traffic locally')
+
+const options = program.opts();
+const destUrl = command.destinationUrl;
+const destPort = command.port;
+
+program.parse(process.argv);
+*/
 
 app.get("/", (req,res) => {
     res.redirect(302, `http://${destUrl}:${destPort}`);
