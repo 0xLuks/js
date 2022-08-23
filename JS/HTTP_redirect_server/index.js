e43d8faf5ef25dfc2e2a9937edf1d38465cfcf4d
Luks <51793738+0xLuks@users.noreply.github.com>
2022-08-23 15:46:35 +0200

    Update index.js

--- JS/HTTP_redirect_server/index.js
+++ JS/HTTP_redirect_server/index.js
@@ -1,1 +1,13 @@
-const blabla
+const http = require('http');
+const program = (require('commander'))
+    .description('Redirects all HTTP traffic locally')
+    .option('-r, --destinationUrl <path>', 'the url that will be redirected')
+    .option('-p, --port <port>, "specify the port of the url", Number, 3000')
+    .parse(process.argv);
+
+var server = http.createServer(function (req, res) {
+    res.writeHead(302, {'Location': program.destinationUrl + req.url});
+    res.end;
+});
+
+server.listen(program.port);
